76b6f6759d470d2c95875e3f1e2f4ec433280e9a
ThilakSparrow <sthilak3636@gmail.com>
2024-03-31 13:15:11 +0530

    Spring topics

--- JWT/src/main/java/io/spring/security/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package io.spring.security.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
--- JWT/src/main/java/io/spring/security/user/Role.java
+++
@@ -1,5 +0,0 @@
-package io.spring.security.user;
-
-public enum Role {
-	USER, ADMIN , MANAGER
-}
